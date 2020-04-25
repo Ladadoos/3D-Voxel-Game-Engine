ceff4814fd7884919984c491c48b609cd76225d9
Dion <dionkamerman@gmail.com>
2020-04-25 20:05:42 +0200

    Added vignette post processing effect.

--- Minecraft/Shaders/PostRenderShader/fs_postRender.glsl
+++ Minecraft/Shaders/PostRenderShader/fs_postRender.glsl
@@ -7,7 +7,20 @@ in vec2 uv;                //Interpolated texture coordinates
 uniform sampler2D colorTexture;
 uniform sampler2D depthNormalTexture;
 
+vec4 vignette(vec4 color){
+	vec2 texSize = textureSize(colorTexture, 0);
+	vec2 relativeToCenter = gl_FragCoord.xy / texSize - 0.5F;
+	float distToCenter = length(relativeToCenter);
+
+	const float innerDistance = 0.4;
+	const float outerDistance = 0.8;
+	const float blendFactor = 0.325;
+	float vignette = smoothstep(outerDistance, innerDistance, distToCenter);
+	return vec4(mix(color.rgb, color.rgb * vignette, blendFactor), 1);
+}   
+
 void main()
 {
-   outputColor = texture(colorTexture, uv);
+	vec4 rawColor = texture(colorTexture, uv);
+	outputColor = vignette(rawColor);
 }
