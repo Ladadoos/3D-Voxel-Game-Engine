9a842fb8a19555ab0a94c616b79e193f3a248a40
Dion <dionkamerman@gmail.com>
2020-05-13 21:28:01 +0200

    Removed player interaction with stone.

--- Minecraft/World/Blocks/Block/BlockStone.cs
+++ Minecraft/World/Blocks/Block/BlockStone.cs
@@ -2,20 +2,11 @@
 {
     class BlockStone : Block
     {
-        public BlockStone(ushort id) : base(id)
-        {
-            isInteractable = true;
-        }
+        public BlockStone(ushort id) : base(id) { }
 
         public override BlockState GetNewDefaultState()
         {
             return new BlockStateStone();
-        }
-
-        public override void OnInteract(BlockState blockstate, Vector3i blockPos, World world)
-        {
-            BlockStateStone state = (BlockStateStone)blockstate;
-            Logger.Info("Interacted with stone");
         }
     }
 }
