d08570b85667160f7d3d590b99db7d96ebb4b252
Dion <dionkamerman@gmail.com>
2020-05-25 00:57:55 +0200

    Cleaned vector extensions and added Vector2 extensions

---
+++ Minecraft/Tools/Vector/Vector2Extensions.cs
@@ -0,0 +1,13 @@
+﻿using OpenTK;
+using System;
+
+namespace Minecraft
+{
+    static class Vector2Extensions
+    {
+        public static Vector2 ManhattanDistance(this Vector2 thisVec, Vector2 otherVec)
+        {
+            return new Vector2(Math.Abs(thisVec.X - otherVec.X) + Math.Abs(thisVec.Y - otherVec.Y);
+        }
+    }
+}
---
+++ Minecraft/Tools/Vector/Vector3Extensions.cs
@@ -0,0 +1,17 @@
+﻿using OpenTK;
+
+namespace Minecraft
+{
+    static class Vector3Extensions
+    {
+        public static Vector3 Plus(this Vector3 vector, Vector3i vectorI)
+        {
+            return new Vector3(vector.X + vectorI.X, vector.Y + vectorI.Y, vector.Z + vectorI.Z);
+        }
+
+        public static Vector3 Plus(this Vector3 vector, float value)
+        {
+            return new Vector3(vector.X + value, vector.Y + value, vector.Z + value);
+        }
+    }
+}
